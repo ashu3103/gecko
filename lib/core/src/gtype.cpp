57c6e5c3991c9fa41545dc879f32095a16f398ed
ashu3103 <ash2003sharma@gmail.com>
2025-10-16 15:13:56 +0530

    project setup cmakes

--- lib/core/src/gtype.cpp
+++ lib/core/src/gtype.cpp
@@ -2,10 +2,22 @@
 #include <cmath>
 
 namespace core {
-    std::string stringify(const gtype& a)
+    std::string stringify(const gtype& value)
     {
-        // TODO(ashu3103)
-        return "";
+        return std::visit([](const auto& v) -> std::string {
+        using T = std::decay_t<decltype(v)>;
+        if constexpr (std::is_same_v<T, std::monostate>) {
+            return "nil"; // or "null"
+        } else if constexpr (std::is_same_v<T, double>) {
+            return std::to_string(v);
+        } else if constexpr (std::is_same_v<T, std::string>) {
+            return v;
+        } else if constexpr (std::is_same_v<T, bool>) {
+            return v ? "true" : "false";
+        } else {
+            return "unknown"; // fallback
+        }
+    }, value);
     }
 
     bool equals(const gtype& a, const gtype& b)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 #include <fstream>
-#include <scanner.h>
+#include <token.h>
+#include <printer.h>
+#include <expr.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    // create a token
+    token::Token tok(TokenType::_BANG, "!", 0, 0);
+    // create a literal
+    core::gtype l = "valid";
+    ast::Literal<std::string> litExpr(l);
+    
+
+    // typecast the unary expr
+    ast::Expr<std::string>* ex = new ast::Unary<std::string>(&litExpr, tok);
+    ast::AstPrinter p;
+    
+    std::cout << ex->accept(p) << std::endl;
+
     return 0;
 }
