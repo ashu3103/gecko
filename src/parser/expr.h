90754f5fd2d89c7025f8be33f811995b47d09743
ashu3103 <ash2003sharma@gmail.com>
2025-10-16 15:13:29 +0530

    generated expr

--- src/parser/expr.h
+++ src/parser/expr.h
@@ -6,46 +6,84 @@
 #include <token.h>
 
 namespace ast {
+	template <typename R>
 	class Binary;
+	template <typename R>
 	class Unary;
+	template <typename R>
+	class Grouping;
+	template <typename R>
+	class Literal;
 
-	template <typename Derived>
+	template <typename R>
+	class Visitor {
+		public:
+			virtual R visitBinaryExpr(Binary<R>* expr) = 0;
+			virtual R visitUnaryExpr(Unary<R>* expr) = 0;
+			virtual R visitGroupingExpr(Grouping<R>* expr) = 0;
+			virtual R visitLiteralExpr(Literal<R>* expr) = 0;
+			virtual ~Visitor() = default;
+	};
+	template <typename R>
 	class Expr {
 		public:
-			template <typename R>
-			class Visitor {
-				virtual R visitBinaryExpr(Binary* expr) = 0;
-				virtual R visitUnaryExpr(Unary* expr) = 0;
-				virtual ~Visitor() = default;
-			};
 			virtual ~Expr() = default;
-			template <typename R>
-			R acceptVirtual(Visitor<R>& visitor) {
-				static_cast<Derived*>(this)->accept(visitor);
+			virtual R accept(Visitor<R>& visitor) = 0;
+	};
+
+	template <typename R>
+	class Binary: public Expr<R> {
+		public:
+			Expr<R>* left;
+			token::Token oper;
+			Expr<R>* right;
+
+			Binary(Expr<R>* left, token::Token oper, Expr<R>* right): left(left), oper(oper), right(right) {
+			}
+
+			R accept(Visitor<R>& visitor) override {
+				return visitor.visitBinaryExpr(this);
 			}
 	};
 
-	class Binary: public Expr<Binary> {
+	template <typename R>
+	class Unary: public Expr<R> {
 		public:
-			Expr left;
+			Expr<R>* right;
 			token::Token oper;
-			Expr right;
 
-			Binary(Expr left, token::Token oper, Expr right);
+			Unary(Expr<R>* right, token::Token oper): right(right), oper(oper) {
+			}
 
-			template <typename R>
-			R accept(Visitor<R>& vis);
+			R accept(Visitor<R>& visitor) override {
+				return visitor.visitUnaryExpr(this);
+			}
 	};
 
-	class Unary: public Expr<Unary> {
+	template <typename R>
+	class Grouping: public Expr<R> {
 		public:
-			Expr mid;
-			token::Token oper;
+			Expr<R>* expr;
 
-			Unary(Expr mid, token::Token oper);
+			Grouping(Expr<R>* expr): expr(expr) {
+			}
 
-			template <typename R>
-			R accept(Visitor<R>& vis);
+			R accept(Visitor<R>& visitor) override {
+				return visitor.visitGroupingExpr(this);
+			}
+	};
+
+	template <typename R>
+	class Literal: public Expr<R> {
+		public:
+			core::gtype lit;
+
+			Literal(core::gtype lit): lit(lit) {
+			}
+
+			R accept(Visitor<R>& visitor) override {
+				return visitor.visitLiteralExpr(this);
+			}
 	};
 
 }
